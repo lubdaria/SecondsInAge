da3093f364b91980e2a4c1b5cd3657317971ffdb
lubdaria <98103429+lubdaria@users.noreply.github.com>
2022-05-05 21:13:14 +0300

    first commit

---
+++ secondsInAge.js
@@ -0,0 +1,20 @@
+let secondsInAMinute = 60;
+let minuteInAnHour = 60;
+
+let secondsInAnHour = secondsInAMinute * minuteInAnHour;
+console.log(secondsInAnHour);
+
+let hourInADay = 24;
+
+let secondInADay = secondsInAnHour * hourInADay;
+console.log(secondInADay);
+
+let dayInAYear = 365;
+
+let secondsInAYear = secondInADay * dayInAYear;
+console.log(secondsInAYear);
+
+let age = 21;
+ 
+let secondsInAge = secondsInAYear * age;
+console.log(secondsInAge);
